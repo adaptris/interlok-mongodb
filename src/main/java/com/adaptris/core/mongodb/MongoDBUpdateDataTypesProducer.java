a80f3a942fcb7587e6ad5b89cdf498439f5cb2cd
Terry Chung <terry.chung@proagrica.com>
2025-01-16 16:41:11 +1100

    INTERLOK-4477: Upgrade to jakarta EE 10e to jakarta EE 10

--- src/main/java/com/adaptris/core/mongodb/MongoDBUpdateDataTypesProducer.java
+++ src/main/java/com/adaptris/core/mongodb/MongoDBUpdateDataTypesProducer.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 
 import org.bson.Document;
 import org.bson.types.ObjectId;
